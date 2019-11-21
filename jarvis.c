570d340ad70121b67cca37c9cad8eb9768668311
Rish7223 <Rish7223@users.noreply.github.com>
2019-11-21 19:45:47 +0530

    jarvis.c

--- jarvis.c
+++ jarvis.c
@@ -1,22 +1,31 @@
 #include <stdio.h>
+#include<process.h>
 #include <stdlib.h>
 #include <string.h>
 #include <conio.h>
 void calculator();  //calculator function prototype.
 void quiz();  //quiz function prototype.
+char name[50];
 void main()
 {
     int pass,ch,ch2;
-    printf("\n\t====================================================");
-    printf("\n\n\t\tHello: Welcome to jarvis version 1. ");
+    printf("\n\t\t====================================================");
+    printf("\n\t\t|      Hello: Welcome to jarvis version 1.         | ");
+    printf("\n\t\t====================================================");
     xy:
-    printf("\n\t\tEnter Password: ");  //asking to enter password.
+    printf("\n\n\t\tEnter Password or 0 to exit: ");  //asking to enter password.
     scanf("%d",&pass);
     if(pass == 3501213)   //checking the correct password.
     {   
-		printf("\n\t\t\tWelcome Rishabh Tyagi!");
+		system("cls");
+		printf("\n\t\t\tEnter Your name: ");
+		scanf("%s",name);
+		printf("\n\n\t\t\tWelcome %s",name);
+		printf("\n\t\t\tPress Any key to Enter: ");
+		getch();
         yz:
-        printf("\n\t\t\tMain Menu");
+        system("cls");
+        printf("\n\n\n\t\t\tMain Menu");
         printf("\n\t\t\t1. Calculator\n\t\t\t2. Play Quiz");
         printf("\n\t\t\t0. Exit!");
         printf("\n\t\t\tEnter 1 for Calculation and 2 for Play a quiz game: ");
@@ -25,7 +34,9 @@ void main()
         {
             case 1:  //case for open calculator
                 calculator();   //calculator function call.
-                printf("\t\t\t1. Main Menu\n\t\t\t0. Exit");
+                system("cls");
+                printf("\n\t\t====================================================");
+                printf("\n\n\t\t\t1. Main Menu\n\t\t\t0. Exit");
                 printf("\n\t\t\tEnter your choise: ");
                 scanf("%d",&ch2);
                 switch (ch2)
@@ -33,13 +44,13 @@ void main()
                     case 1:
                         goto yz;
                     case 0:
-                        break;
+                        goto exit;
                 }
-               
-                
             case 2:
                 quiz();   //quiz function call.
-                printf("\t\t\t1. Main Menu\n\t\t\t0. Exit");
+                system("cls");
+                printf("\n\t\t====================================================\n");
+                printf("\n\t\t\t1. Main Menu\n\t\t\t0. Exit");
                 printf("\n\t\t\tEnter your choise: ");
                 scanf("%d",&ch2);
                 switch (ch2)
@@ -56,6 +67,7 @@ void main()
                 printf("\t\t\tInvalid Option.. Try Again");
                 goto yz;
         }
+        exit:
         printf("\n\t\t\t\t\tThankYou!!!!");
     }
     else if(pass == 0)
@@ -67,81 +79,9 @@ void main()
         goto xy;}
 }
 
-void calculator()     //calculator function defination.
-{
-    float x,y,add,sub,mul,di;
-    int ch;
-    printf("\n\t\t\tWelcome to a two ditit operation calculator");
-    xy:
-    printf("\n\t\t\tEnter Two digits: ");
-    scanf("%f, %f",&x,&y);
-    printf("\t\t\t1. Adition(+)\n\t\t\t2. subtracion(-)");
-    printf("\n\t\t\t3. Multipication\n\t\t\t4. Adition(/)\n\t\t\t0. exit");
-    printf("\n\t\t\tEnter your choice: ");
-    scanf("%d",&ch);
-    switch (ch)
-    {
-        case 1:
-            add = x+y;
-            printf("\t\t\t%.2f",add);
-            goto xy;
-        case 2:
-            sub = x-y;
-            printf("\t\t\t%.2f",sub);
-            goto xy;
-        case 3:
-            mul = x*y;
-            printf("\t\t\t%.2f",mul);
-            goto xy;
-        case 4:
-            di = x/y;
-            printf("\t\t\t%.2f",di);
-            goto xy;
-        case 0:
-            break;
-        default :
-            printf("\t\t\tInvalid option!");
-            goto xy;
-    }
-}
-void quiz()  //Quiz function defination.
-{
-    int ch;
-    xy:
-    printf("\n\t\t=============================================");
-    printf("\n\t\t\tWelcome to Quiz Show");
-    printf("\n\t\t\t1. Computer Quiz\n\t\t\t2. Space Quiz");
-    printf("\n\t\t\t3. General Quiz\n\t\t\t4. Help/Rules\n\t\t\t0. Exit");
-    printf("\n\t\t\tEnter your choise: ");
-    scanf("%d",&ch);
-    switch (ch)
-    {
-        case 1:
-        	printf("\n\t\tComing Soon!  Sorry..");
-        	goto xy;
-        case 2:
-        	printf("\n\t\tComing Soon!  Sorry..");
-        	goto xy;
-        case 3:
-        	printf("\n\t\tComing Soon!  Sorry..");
-        	goto xy;
-		case 4:
-            printf("\t\t\tRULES of the QUIZ GAME:-");
-            printf("\n\t\t\t-> There are total 5 quizs related to your quiz.");
-            printf("\n\t\t\t-> Every correct answer will give you 1 point.");
-            printf("\n\t\t\t-> Result will be shown you at the end.");
-            printf("\n\t\t\t-> Happy Playing\n");
-            printf("\n\n\t\t\tEnter Any Key to Exit: ");
-            getch();
-            goto xy;
-        case 0:
-            break;
-        default:
-            printf("\t\t\tInvalid option! try again.");
-            goto xy;
-            
-    }
-}
+
+
+
 
 
 
